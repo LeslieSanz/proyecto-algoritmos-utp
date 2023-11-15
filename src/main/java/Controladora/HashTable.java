b8bf1e91ec284e459e2fc8fdf499c94fba26f4fb
LuisMPS <135759358+LuisMPS17@users.noreply.github.com>
2023-11-14 22:25:40 -0500

    Hashtable

---
+++ src/main/java/Controladora/HashTable.java
@@ -0,0 +1,42 @@
+package Controladora;
+
+import Clases.cLibro;
+import java.util.HashMap;
+
+/**
+ *
+ * @author luism
+ */
+public class HashTable {
+    
+    private HashMap<Integer, cLibro> hashtable;
+
+    public HashTable() {
+        
+        hashtable = new HashMap<>();
+    } 
+    
+    public void insertarObjeto(cLibro libro){
+        
+        hashtable.put(libro.getISBN(), libro);
+              
+    }
+    
+    public cLibro recuperarObjeto(int ISBN) {
+        
+        return hashtable.get(ISBN);
+        
+    }
+    
+    public void listarObjetos(){
+        for (cLibro libro : hashtable.values()){
+            System.out.println(libro.mostrar());
+        }
+    }
+
+    
+
+    
+    
+    
+}
